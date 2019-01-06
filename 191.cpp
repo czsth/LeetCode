a680e0854973ccbada73ba8d477fdd25d670dced
czsth <34812091+czsth@users.noreply.github.com>
2019-01-06 20:37:32 +0800

    Add files via upload

---
+++ 191.cpp
@@ -0,0 +1,18 @@
+//这是邓俊辉在数据结构习题集里的代码
+//以n的二进制位数r作为问题规模，该算法复杂度为O(log r)
+//原因在于这是一个分治算法
+#define POW(c) (1 << (c))
+#define MASK(c) (((unsigned long) -1) / (POW(POW(c)) + 1))
+#define ROUND(n, c) (((n) & MASK(c)) + ((n) >> POW(c) & MASK(c)))
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        n = ROUND(n, 0);
+        n = ROUND(n, 1);
+        n = ROUND(n, 2);
+        n = ROUND(n, 3);
+        n = ROUND(n, 4);
+        return n;
+    }
+};
