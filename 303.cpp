1ea45084c38974c4f8c236db5237205e706e97ff
czsth <34812091+czsth@users.noreply.github.com>
2019-01-17 15:21:02 +0800

    Add files via upload

---
+++ 303.cpp
@@ -0,0 +1,23 @@
+class NumArray {
+private:
+    vector<int> sums;
+public:
+    NumArray(vector<int> nums) {
+        if (nums.size() == 0) return;
+        sums.push_back(nums[0]);
+        for (int i = 1; i < nums.size(); ++i) {
+            sums.push_back(nums[i] + sums[i - 1]);
+        }
+    }
+    
+    int sumRange(int i, int j) {
+        if (i == 0) return sums[j];
+        else return sums[j]-sums[i-1];
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(i,j);
+ */
