7c662fa2fe9df3cb9606eae60ee82540920f1d2b
czsth <34812091+czsth@users.noreply.github.com>
2019-01-29 14:06:09 +0800

    Add files via upload

---
+++ 461.py
@@ -0,0 +1,9 @@
+# 意思就是这个意思，就用python写了
+class Solution:
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        return bin(x ^ y).count('1')
